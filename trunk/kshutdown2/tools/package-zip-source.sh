7dc4be82c6c2d8198cce8abdcdc7875a9d8d657c
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2008-02-10 23:51:04 +0000

    Fixed source package

--- trunk/kshutdown2/tools/package-zip-source.sh
+++ trunk/kshutdown2/tools/package-zip-source.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-function ks_copy()
-{
-	mkdir "$KS_DIR$1"
-	cp .$1/* "$KS_DIR$1"
-}
-
 if [ ! -f "./tools/make-version.sh" ]; then
 	echo "Usage: $0"
 	exit 1
 fi
 
-# version
-./tools/make-version.sh
+# make version number
 
+./tools/make-version.sh
 KS_VERSION=`cat tools/VERSION`
+
+# init variables
+
 KS_DIR="kshutdown-$KS_VERSION"
 KS_DIST_DIR="dist.tmp"
 KS_ZIP="kshutdown-src-$KS_VERSION.zip"
 
-# clean
+# clean before copy
+
 rm -fR "$KS_DIR"
 rm -fR "$KS_DIST_DIR"
 
@@ -29,14 +27,18 @@ rm ./kshutdown
 rm ./Makefile
 popd
 
-ks_copy ""
-ks_copy "/src"
-ks_copy "/src/images"
-ks_copy "/tools"
+# copy source files
+
+mkdir "$KS_DIR"
+cp * "$KS_DIR"
+cp -r "patches" "$KS_DIR"
+cp -r "src" "$KS_DIR"
+cp -r "tools" "$KS_DIR"
 
 # zip and checksum
+
 mkdir "$KS_DIST_DIR"
-zip -r9 "$KS_DIST_DIR/$KS_ZIP" "$KS_DIR" -x "*~"
+zip -r9 "$KS_DIST_DIR/$KS_ZIP" "$KS_DIR" -x "*~" -x "*/.svn/*"
 pushd "$KS_DIST_DIR"
 sha1sum "$KS_ZIP">SHA1SUM
 popd
