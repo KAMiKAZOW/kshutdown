9830101252c2e54b0887ede4e349dd9ac89c9d86
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2008-02-10 20:01:16 +0000

    Setup-kde4.sh: Added option to specify installation directory (prefix). Example: ./Setup-kde4.sh /usr/local

--- Setup-kde4.sh
+++ Setup-kde4.sh
@@ -2,20 +2,34 @@
 
 # TODO: uninstall: http://www.cmake.org/Wiki/CMake_FAQ
 
-set -e
+echo "TIP: Run \"$0 /your/prefix/dir\" to specify custom installation directory"
 
-PREFIX=`kde4-config --prefix`
+PREFIX="$1"
+BUILD_TYPE=Release
+
 if [ -z "$PREFIX" ]; then
-	PREFIX=/usr/local
+	KDE4_CONFIG=$(which kde4-config)
+	if [ -z "$KDE4_CONFIG" ]; then
+		echo "WARNING: \"kde4-config\" not found; using default installation prefix"
+		PREFIX=/usr/local
+	else
+		PREFIX=`$KDE4_CONFIG --prefix`
+		if [ -z "$PREFIX" ]; then
+			PREFIX=/usr/local
+		fi
+	fi
 fi
+
+set -e
 
 BUILD_DIR="build.tmp"
 rm -fR "$BUILD_DIR"
 mkdir "$BUILD_DIR"
 
-BUILD_TYPE=Release
-
 pushd "$BUILD_DIR"
+
+echo "INFO: Installation prefix: $PREFIX"
+echo "INFO: Build type         : $BUILD_TYPE"
 
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
 make
