2fdb4637a7771a875960fdf87fd84c557eae8779
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2008-11-14 21:54:36 +0000

    Updated Qt libraries to version 4.4.3

--- trunk/kshutdown2/Setup-qt4.bat
+++ trunk/kshutdown2/Setup-qt4.bat
@@ -1,4 +1,4 @@
-call C:\Qt\4.3.2\bin\qtvars.bat
+call C:\Qt\4.4.3\bin\qtvars.bat
 cd src
 
 rem goto skip_portable
@@ -27,6 +27,6 @@ cd ..
 if not %errorlevel% == 0 goto quit
 kshutdown-2.0beta3-win32.exe
 
-copy C:\Qt\4.3.2\bin\mingwm10.dll kshutdown-portable
+copy C:\Qt\4.4.3\bin\mingwm10.dll kshutdown-portable
 
 :quit
