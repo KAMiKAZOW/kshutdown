f3911e6e378c7cc7b20cd2f19e7bec881e0e87ee
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2009-02-21 06:59:00 +0000

    Added GNU GPL info

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -701,7 +701,17 @@ void MainWindow::onAbout() {
 		KS_COPYRIGHT "<br>" \
 		"<br>" \
 		"<a href=\"" KS_HOME_PAGE "\">" KS_HOME_PAGE "</a><br>" \
-		"<a href=\"mailto:" KS_CONTACT "\">" KS_CONTACT "</a>" \
+		"<a href=\"mailto:" KS_CONTACT "\">" KS_CONTACT "</a><br>" \
+		"<br>" \
+"This program is free software; you can redistribute it and/or modify<br>" \
+"it under the terms of the <a href=\"http://www.gnu.org/licenses/\">GNU General Public License</a> as published by<br>" \
+"the Free Software Foundation; either version 2 of the License, or<br>" \
+"(at your option) any later version.<br>" \
+"<br>" \
+"This program is distributed in the hope that it will be useful,<br>" \
+"but WITHOUT ANY WARRANTY; without even the implied warranty of<br>" \
+"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br>" \
+"GNU General Public License for more details." \
 		"</qt>"
 	);
 }
