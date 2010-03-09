729a540b532daf5bfcb9e20ff798bf4c5b329d65
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2010-03-09 14:40:49 +0000

    An initial implementation of user-activity monitor (X11)

--- src/triggers/idlemonitor.cpp
+++ src/triggers/idlemonitor.cpp
@@ -15,161 +15,159 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+#ifdef Q_WS_WIN
+	// !!!
+#else
+	#include <QDBusInterface>
+ 	#include <QDBusReply>
+#endif // Q_WS_WIN
+
+#include <QDateTimeEdit>
+
 #include "idlemonitor.h"
 
 // public
 
 IdleMonitor::IdleMonitor()
-	: KShutdown::Trigger(i18n("Monitor user inactivity"), "user-away", "idle-monitor")
-//	m_widget(0),
+	: KShutdown::DateTimeTriggerBase(
+		i18n("On User Inactivity (HH:MM)") + " [Experimental]",
+		"user-away-extended",//!!!user-idle?
+		"idle-monitor"
+	),
+	m_idleTime(0)
 {
-	m_checkTimeout = 2000;
+	//!!!m_checkTimeout = 5000;
+	
+	m_checkTimeout = 1000;
+	
+#ifdef Q_WS_WIN
+	m_supported = true;
+#else
+	m_dbus = new QDBusInterface(
+		"org.freedesktop.ScreenSaver",
+		"/ScreenSaver",
+		"org.freedesktop.ScreenSaver"
+	);
+	m_supported = m_dbus->isValid();
+#endif // Q_WS_WIN
+
+	setWhatsThis(i18n("Use this trigger to detect user inactivity (no mouse or keyboard input)."));
+}
+
+IdleMonitor::~IdleMonitor() {
+	if (m_dbus) {
+		delete m_dbus;
+		m_dbus = 0;
+	}
 }
 
 bool IdleMonitor::canActivateAction() {
-	return false;
-/* !!!	if (m_processList.isEmpty())
+	getSessionIdleTime();
+	
+	if (m_idleTime == 0) {
+		m_status = i18n("Unknown");
+		
 		return false;
+	}
 
-	int index = m_processes->currentIndex();
-	Process *p = m_processList.value(index);
-	m_status = i18n("Waiting for \"%0\"")
-		.arg(p->toString());
+	QTime time = m_dateTime.time();
+	quint32 timeout = ((time.hour() * 60) + time.minute()) * 60;
+	U_DEBUG << "timeout=" << timeout U_END;
+	
+	if (m_idleTime >= timeout)
+		return true;
 
-	return !p->isRunning();*/
+	quint32 remainingTime = timeout - m_idleTime;
+	QTime t = QTime();
+	m_status = "~" + t.addSecs(remainingTime).toString("HH:mm:ss");
+	
+	m_status += (" {DEBUG:" + QString::number(m_idleTime) + "}");
+
+	return false;
 }
 
 QWidget *IdleMonitor::getWidget() {
-	/* !!! if (!m_widget) {
-		m_widget = new QWidget();
-		QHBoxLayout *layout = new QHBoxLayout(m_widget);
-		layout->setMargin(0);
-		layout->setSpacing(5);
+	if (!m_edit) {
+		DateTimeTriggerBase::getWidget();
 
-		m_processes = new U_COMBO_BOX(m_widget);
-		m_processes->setFocusPolicy(Qt::StrongFocus);
-		m_processes->setToolTip(i18n("List of the running processes"));
-		layout->addWidget(m_processes);
-		
-		U_PUSH_BUTTON *refreshButton = new U_PUSH_BUTTON(m_widget);
-#ifdef KS_NATIVE_KDE
-		refreshButton->setIcon(U_STOCK_ICON("view-refresh"));
-#else
-		refreshButton->setText(i18n("Refresh"));
-#endif // KS_NATIVE_KDE
-		refreshButton->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Preferred));
-		refreshButton->setToolTip(i18n("Refresh the list of processes"));
-		connect(
-			refreshButton, SIGNAL(clicked()),
-			SLOT(onRefresh())
-		);
-		layout->addWidget(refreshButton);
+		m_edit->setDisplayFormat(KShutdown::TIME_FORMAT);
+		m_edit->setTime(m_dateTime.time());
+		m_edit->setToolTip(i18n("Enter a maximum user inactivity in \"HH:MM\" format (Hour:Minute)"));
 	}
-	onRefresh();
 
-	return m_widget;*/
-	return 0;
+	return m_edit;
 }
 
-// public slots
+void IdleMonitor::setState(const State state) {
+	Q_UNUSED(state)
+	
+	if (state == StartState) {
+		m_idleTime = 0;
 
-/* !!! 
-void ProcessMonitor::onRefresh() {
-#ifdef KS_TRIGGER_PROCESS_MONITOR
-	m_processes->clear();
-	m_processes->setEnabled(true);
-	m_processList.clear();
-	m_refreshBuf = QString::null;
+#ifdef Q_WS_X11
+		if (m_supported)
+			m_dbus->call("SimulateUserActivity");//!!!
+#endif // Q_WS_X11
+	}
+	else if (state == StopState) {
+		m_idleTime = 0;
+	}
+}
 
-	if (!m_refreshProcess) {
-		m_refreshProcess = new QProcess(this);
-		connect(
-			m_refreshProcess, SIGNAL(error(QProcess::ProcessError)),
-			SLOT(onError(QProcess::ProcessError))
-		);
-		connect(
-			m_refreshProcess, SIGNAL(finished(int, QProcess::ExitStatus)),
-			SLOT(onFinished(int, QProcess::ExitStatus))
-		);
-		connect(
-			m_refreshProcess, SIGNAL(readyReadStandardOutput()),
-			SLOT(onReadyReadStandardOutput())
-		);
+// protected
+
+/**
+ * The returned value is unused in this trigger;
+ * just return something...
+ */
+QDateTime IdleMonitor::calcEndTime() {
+	return m_edit->dateTime();
+}
+
+/**
+ * Sets @c null status.
+ */
+void IdleMonitor::updateStatus() {
+	m_status = QString::null;
+}
+
+// private
+
+/**
+ * Sets the @c m_idleTime to the current session idle time (in seconds).
+ * Sets @c zero if this information is unavailable.
+ */
+void IdleMonitor::getSessionIdleTime() {
+	if (!m_supported) {
+		m_idleTime = 0;
+		
+		return;
+	}
+
+#ifdef Q_WS_WIN
+	LASTINPUTINFO lii;
+	lii.cbSize = sizeof(LASTINPUTINFO);
+	BOOL result = ::GetLastInputInfo();
+	if (result) {
+		qulong64 tickCount = ::GetTickCount();
+		qulong64 lastTick = lii.dwTime;
+
+		// HACK: avoid rollback <http://en.wikipedia.org/wiki/GetTickCount#Rollback>
+		m_idleTime = ((tickCount - lastTick) & 0xFFFFFFFF) / 1000;
 	}
 	else {
-		if (m_refreshProcess->state() == QProcess::Running)
-			m_refreshProcess->terminate();
+		m_idleTime = 0;
 	}
+#else
+	QDBusReply<quint32> reply = m_dbus->call("GetSessionIdleTime");
 	
-	QStringList args;
-	// show all processes
-	args << "-A";
-	// order: user pid command
-	args << "-o" << "user=";
-	args << "-o" << "pid=";
-	args << "-o" << "comm=";
-	// sort by command
-	args << "--sort" << "comm";
+	if (reply.isValid()) {
+		U_DEBUG << "org.freedesktop.ScreenSaver: reply=" << reply.value() U_END;
 
-	// start process
-	m_refreshProcess->start("ps", args);
-#endif // KS_TRIGGER_PROCESS_MONITOR
-}
-
-// private slots
-
-void ProcessMonitor::onError(QProcess::ProcessError error) {
-	errorMessage(i18n("Error: %0").arg(error));
-}
-
-void ProcessMonitor::onFinished(int exitCode, QProcess::ExitStatus exitStatus) {
-#ifdef KS_TRIGGER_PROCESS_MONITOR
-	U_DEBUG << "ProcessMonitor::onFinished( exitCode=" << exitCode << ", exitStatus=" << exitStatus << " )" U_END;
-	
-	if (exitStatus == QProcess::NormalExit) {
-		int index = 0;
-		int line = 0;
-		Process *p = 0;
-		QString user = Utils::getUser();
-		foreach (QString i, m_refreshBuf.simplified().split(" ")) {
-			switch (line) {
-				// user
-				case 0:
-					line++; // next is pid
-					p = new Process(this);
-					p->m_user = i;
-					break;
-				// pid
-				case 1:
-					line++; // next is command
-					p->m_pid = i.toLong();
-					break;
-				// command
-				case 2:
-					line = 0; // next is user (wrap around)
-					p->m_command = i;
-					m_processList.append(p);
-					m_processes->addItem(
-						// show icons for the current user only (faster)
-						(p->m_user == user) ? U_STOCK_ICON(p->m_command) : U_ICON(),
-						p->toString(),
-						index
-					);
-					break;
-			}
-			index++;
-		}
-		
-		if (m_processList.isEmpty())
-			errorMessage(i18n("Error, exit code: %0").arg(exitCode));
+		m_idleTime = reply.value();
 	}
-	else { // QProcess::CrashExit
-		errorMessage(i18n("Error, exit code: %0").arg(exitCode));
+	else {
+		m_idleTime = 0;
 	}
-#endif // KS_TRIGGER_PROCESS_MONITOR
+#endif // Q_WS_WIN
 }
-
-void ProcessMonitor::onReadyReadStandardOutput() {
-	m_refreshBuf.append(m_refreshProcess->readAllStandardOutput());
-}
-*/
