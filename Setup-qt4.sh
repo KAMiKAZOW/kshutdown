907ad7f906ac209d914116697ecb92eb822b33a5
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2010-05-30 18:11:13 +0000

    Added support for Qt4-only Builds (see Setup-qt4.sh)

--- Setup-qt4.sh
+++ Setup-qt4.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-#
-
-# !!!
-
-echo "SORRY, UNDER CONSTRUCTION!"
-exit 1
+function ks_info()
+{
+	echo
+	echo "INFO: $1"
+	echo
+}
 
 pushd "src"
+
 QMAKE=`which qmake-qt4`
 if [ -z "$QMAKE" ]; then
 	QMAKE=qmake
 fi
 
 set -e
+
+ks_info "Configuring project..."
 $QMAKE -config release
+
+ks_info "Cleaning project..."
 make clean
+
+ks_info "Compiling... This may take a few minutes ;)"
 make
+
 popd
+
+cp src/kshutdown .
+ks_info "See the \"$(pwd)\" directory; no installation required:"
+ls -lh kshutdown
+echo
