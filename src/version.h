2f14c0edfe2d2f022c383d79ef96128c1cc00c31
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2010-08-27 12:41:33 +0000

    Updated ChangeLog and version info

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 #ifndef KSHUTDOWN_VERSION_H
 #define KSHUTDOWN_VERSION_H
 
-#define KS_BUILD "20100722"
-#define KS_FILE_VERSION "2.0beta10"
-#define KS_FULL_VERSION "2.0 Beta 10"
+#define KS_BUILD "20100827"
+#define KS_FILE_VERSION "2.0beta11"
+#define KS_FULL_VERSION "2.0 Beta 11"
 
 #endif // KSHUTDOWN_VERSION_H
