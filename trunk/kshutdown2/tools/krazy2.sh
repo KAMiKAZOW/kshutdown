b31aca89975349de1b1adb77e866c8335714cdc7
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2011-04-22 21:58:24 +0000

    Added support for Krazy2 Code Checker (see ./tools/krazy2.sh)

---
+++ trunk/kshutdown2/tools/krazy2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# DOC: http://techbase.kde.org/Development/Tutorials/Code_Checking
+
+if [ ! -f "ChangeLog" ]; then
+	echo "Usage: ./tools/krazy2.sh [directory to test]"
+	echo "Example: ./tools/krazy2.sh src"
+	exit 1
+fi
+
+export PATH=$PATH:/usr/local/Krazy2/bin
+
+dir="$1"
+if [ -z "$dir" ]; then
+	dir="."
+fi
+
+pushd "$dir"
+krazy2all
+popd
