0498dd8043c9f2a0409c413785ca860090ba8fcb
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2011-07-11 19:46:30 +0000

    More human-readable time display format

--- trunk/kshutdown2/src/kshutdown.h
+++ trunk/kshutdown2/src/kshutdown.h
@@ -42,7 +42,7 @@ class QDateTimeEdit;
 namespace KShutdown {
 	
 const QString DATE_TIME_DISPLAY_FORMAT = "MMM d dddd hh:mm";
-const QString TIME_DISPLAY_FORMAT = "hh:mm";
+const QString TIME_DISPLAY_FORMAT = "hh'h ':' 'mm'm'";
 const QString TIME_PARSE_FORMAT = "h:mm";
 
 class U_EXPORT Base {
