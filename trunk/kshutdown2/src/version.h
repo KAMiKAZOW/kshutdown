8590f7f5478df414b45f82ec4e2bd3beef8035c2
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2011-11-24 06:04:47 +0000

    Version change: 2.1.1 Beta -> 3.0 Beta 2 (yes, 3.0 :)

--- trunk/kshutdown2/src/version.h
+++ trunk/kshutdown2/src/version.h
@@ -2,8 +2,8 @@
 #ifndef KSHUTDOWN_VERSION_H
 #define KSHUTDOWN_VERSION_H
 
-#define KS_BUILD "20111021"
-#define KS_FILE_VERSION "2.1.1beta"
-#define KS_FULL_VERSION "2.1.1 Beta"
+#define KS_BUILD "20111124"
+#define KS_FILE_VERSION "3.0beta2"
+#define KS_FULL_VERSION "3.0 Beta 2"
 
 #endif // KSHUTDOWN_VERSION_H
