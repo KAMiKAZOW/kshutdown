56715d28f7f9deefb7e84d781940b37da862174e
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2011-12-23 22:38:13 +0000

    Speed up <windows.h> compilation

--- trunk/kshutdown2/src/actions/lock.cpp
+++ trunk/kshutdown2/src/actions/lock.cpp
@@ -19,6 +19,9 @@
 #include "../utils.h"
 
 #ifdef Q_WS_WIN
+	#ifndef WIN32_LEAN_AND_MEAN
+		#define WIN32_LEAN_AND_MEAN
+	#endif // WIN32_LEAN_AND_MEAN
 	#define _WIN32_WINNT 0x0500 // for LockWorkStation, etc
 	#include <windows.h>
 #else
