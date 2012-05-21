6082b711fd38c1c92ac420f24c4d055c4e40bb9b
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2012-05-21 05:30:21 +0000

    Create a smaller portable package compressed using 7z

--- trunk/kshutdown2/Setup-wine.sh
+++ trunk/kshutdown2/Setup-wine.sh
@@ -3,4 +3,7 @@
 set -e
 
 wineconsole Setup-qt4.bat
+
+rm -f kshutdown-portable/kshutdown.ini
 zip -r9 kshutdown-portable-3.0beta5-win32.zip kshutdown-portable
+7z a kshutdown-portable-3.0beta5-win32.7z kshutdown-portable
