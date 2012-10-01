1b8f236367b0b89da150a146416bf16abe132fd2
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2012-10-01 13:03:15 +0000

    Helper test script for Wine

---
+++ trunk/kshutdown2/tools/test-wine.bat
@@ -0,0 +1,10 @@
+
+rem Usage: wineconsole tools/test-wine.bat
+
+call C:\Qt\4.8.1\bin\qtvars.bat
+cd src
+qmake -config release
+mingw32-make.exe
+cd ..
+rem "C:\Program Files\NSIS\makensis.exe" kshutdown.nsi
+src\release\kshutdown-qt.exe
