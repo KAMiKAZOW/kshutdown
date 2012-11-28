b048330a0dce509f8e05c06948020a8eee5a3446
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2012-11-28 00:57:47 +0000

    Version change: 3.0 Beta 5 -> 3.0 Beta 6

--- trunk/kshutdown2/Setup-wine.sh
+++ trunk/kshutdown2/Setup-wine.sh
@@ -4,6 +4,7 @@ set -e
 
 wineconsole Setup-qt4.bat
 
+KS_FILE_VERSION=`sed 1!d tools/VERSION`
 rm -f kshutdown-portable/kshutdown.ini
-zip -r9 kshutdown-portable-3.0beta5-win32.zip kshutdown-portable
-7z a kshutdown-portable-3.0beta5-win32.7z kshutdown-portable
+zip -r9 "kshutdown-portable-${KS_FILE_VERSION}-win32.zip" kshutdown-portable
+7z a "kshutdown-portable-${KS_FILE_VERSION}-win32.7z" kshutdown-portable
