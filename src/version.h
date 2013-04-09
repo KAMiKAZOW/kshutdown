4c884bebbb7bcba25cfd03fe4e7d1607da16c797
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2013-04-09 13:40:46 +0000

    KShutdown 3.0 Beta 7

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #ifndef KSHUTDOWN_VERSION_H
 #define KSHUTDOWN_VERSION_H
 
-#define KS_BUILD "20130217"
+#define KS_BUILD "20130409"
 #define KS_FILE_VERSION "3.0beta7"
 #define KS_FULL_VERSION "3.0 Beta 7"
 
