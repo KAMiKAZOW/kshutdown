1cb4f5304c70257031d2b606b7899a6dc7b5ef1b
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2013-09-09 11:04:39 +0000

    version_number++ (3.0 -> 3.1 Beta)

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 #ifndef KSHUTDOWN_VERSION_H
 #define KSHUTDOWN_VERSION_H
 
-#define KS_BUILD "20130709"
-#define KS_FILE_VERSION "3.0"
-#define KS_FULL_VERSION "3.0"
+#define KS_BUILD "20130909"
+#define KS_FILE_VERSION "3.1beta"
+#define KS_FULL_VERSION "3.1 Beta"
 
 #endif // KSHUTDOWN_VERSION_H
