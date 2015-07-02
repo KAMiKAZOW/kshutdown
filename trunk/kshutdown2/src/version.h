5dda6227c020b87027c84ca484f1979c0e46fd53
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2015-07-02 17:15:04 +0000

    KShutdown 3.3.1 Beta #gold

--- trunk/kshutdown2/src/version.h
+++ trunk/kshutdown2/src/version.h
@@ -2,8 +2,8 @@
 #ifndef KSHUTDOWN_VERSION_H
 #define KSHUTDOWN_VERSION_H
 
-#define KS_BUILD "2014-12-02"
-#define KS_FILE_VERSION "3.3beta"
-#define KS_FULL_VERSION "3.3 Beta"
+#define KS_BUILD "2015-07-02"
+#define KS_FILE_VERSION "3.3.1beta"
+#define KS_FULL_VERSION "3.3.1 Beta"
 
 #endif // KSHUTDOWN_VERSION_H
