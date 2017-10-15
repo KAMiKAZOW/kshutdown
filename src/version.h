67555bdf42e0008b964e59011e996b7b9dbe00a1
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2017-10-15 13:09:18 +0000

    Update the version numbers (4.1.1 Beta -> 4.2)

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 #ifndef KSHUTDOWN_VERSION_H
 #define KSHUTDOWN_VERSION_H
 
-#define KS_FILE_VERSION "4.1.1beta"
-#define KS_FULL_VERSION "4.1.1 Beta"
-#define KS_RELEASE_DATE "2017-08-24"
+#define KS_FILE_VERSION "4.2"
+#define KS_FULL_VERSION "4.2"
+#define KS_RELEASE_DATE "2017-10-15"
 
 #endif // KSHUTDOWN_VERSION_H
