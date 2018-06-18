c7d77fb762bcea8e27a9a5638e5e6e2417752e76
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2018-06-18 14:06:51 +0000

    * Added Setup-qt5.bat * Removed Setup-qt4.bat and Setup-qt4.sh * Setup-wine.sh: Use a separated Wine profile (~/.wine-kshutdown) and force 32-bit architecture * Win32: Build using Qt 5.11.0 * Installer: Use Unicode (requires NSIS 3.x) * Merge

--- Setup-qt5.sh
+++ Setup-qt5.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-./Setup-qt4.sh -qt5
+function ks_info()
+{
+	echo
+	echo "INFO: $1"
+	echo
+}
+
+pushd "src"
+
+set -e
+
+ks_info "Configuring..."
+qmake -config release
+
+ks_info "Cleaning..."
+make clean
+
+ks_info "Compiling..."
+make -j2
+
+popd
