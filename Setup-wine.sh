b19fdb4a168e541007b766681e186a28244c37cb
kdt <kdt@e700de62-5cb2-4169-922e-98fe9dc2018b>
2019-05-08 07:03:26 +0000

    * Removed "test-wine.bat"; use "Setup-wine.sh" instead * Removed obsolete "kshutdown.nsh" file (version number is now passed as /DAPP_VERSION option) * Updated Wine scripts

--- Setup-wine.sh
+++ Setup-wine.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+KS_BAT_COMMAND="$1"
+
+if [ -z "$KS_BAT_COMMAND" ]; then
+	echo "Usage: ./Setup-wine.sh <command>"
+	echo "Commands:"
+	echo "all   - build everything"
+	echo "setup - build installer for Windows"
+	echo "test  - compile and run"
+	exit 1
+fi
+
 export WINEARCH=win32
 export WINEPREFIX=~/.wine-kshutdown
 
+KS_APP_VERSION=$(sed 1!d VERSION)
+KS_QT_VERSION=$(sed 5!d VERSION)
+
 pushd ~/.wine-kshutdown/drive_c/kshutdown2
-wineconsole Setup-qt5.bat
+wineconsole Setup-qt5.bat "$KS_APP_VERSION" "$KS_QT_VERSION" "$KS_BAT_COMMAND"
 popd
 
-KS_FILE_VERSION=$(sed 1!d VERSION)
 rm -f kshutdown-portable/kshutdown.ini
-7z a "kshutdown-portable-${KS_FILE_VERSION}-win32.7z" kshutdown-portable
+7z a "kshutdown-portable-${KS_APP_VERSION}-win32.7z" kshutdown-portable
